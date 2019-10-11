a2e22681bb1ae685fdc095b3c1dded2e59d6155b
jpkmiller <j.mueller5021@gmail.com>
2019-10-11 16:55:55 +0200

    rewrote api_task2.c

--- 02_Task/api_task2.c
+++ 02_Task/api_task2.c
@@ -5,26 +5,29 @@
  #include <fcntl.h>
  #include <sys/wait.h>
 
-int main(int argc, char *argv[]) {
-   int rc = fork();
-   if (rc < 0) {
-       // fork failed
-       fprintf(stderr, "fork failed\n");
-       exit(1);
-   } else if (rc == 0) {
-       // child: redirect standard output to a file
-       close(STDOUT_FILENO);
-       open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+int main(void) {
+	int fd = open("./task02.txt", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+	char *buf1 = "I am parent";
+	char *buf2 = "I am child";
+   	
+	if (fd == -1)
+	{
+		perror("file could not be opened");
+		exit(1);
+	}
 
-       // now exec "wc"...
-       char *myargs[3];
-       myargs[0] = strdup("wc"); // program: wc (word count)
-       myargs[1] = strdup("p4.c"); // arg: file to count
-       myargs[2] = NULL; // mark end of array
-       execvp(myargs[0], myargs); // runs word count
-       } else {
-       // parent goes down this path (main)
-       int rc_wait = wait(NULL);
-  }
- return 0;
- }
+	int rc = fork();
+   	if (rc < 0) {
+       		// fork failed
+       		fprintf(stderr, "fork failed\n");
+       		exit(1);
+   	} else if (rc == 0) {
+       		write(fd, buf, strlen(buf));
+       	} else {
+		write(fd, buf, strlen(buf));
+       		// parent goes down this path (main)
+       		int rc_wait = wait(NULL);
+	}
+	return 0;
+}
+
