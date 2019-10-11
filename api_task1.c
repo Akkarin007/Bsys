b74fda9b0550e81b32e1aa8054ce6baf200114bf
jpkmiller <j.mueller5021@gmail.com>
2019-10-11 15:58:58 +0200

    updated api_task1.c

--- api_task1.c
+++ api_task1.c
@@ -10,14 +10,14 @@ int main(void)
 	int x = 69;
 	int rc = fork();
 	if (rc < 0) {
-		fprintf(stderr, "fork failed\n");
+		printf(stderr, "fork failed\n");
 		exit(1);
 	} else if (rc == 0) {
 		x = 42;
-		fprintf("%d\n", x);
+		printf("%d\n", x);
 	} else {
 		x = 100;
-		fprintf("%d\n", x);
+		printf("%d\n", x);
 	}
 	return 0;
 }
