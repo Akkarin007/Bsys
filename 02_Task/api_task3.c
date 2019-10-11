96bfda1c0c84d0cf771e6fd6d94d0a6dbbeff48a
Ismail Zeybek <isyzeybek@outlook.com>
2019-10-11 16:58:47 +0200

    Task3 Goodbye and Hello included without order

--- 02_Task/api_task3.c
+++ 02_Task/api_task3.c
@@ -5,26 +5,21 @@
  #include <fcntl.h>
  #include <sys/wait.h>
 
-int main(int argc, char *argv[]) {
-   int rc = fork();
-   if (rc < 0) {
-       // fork failed
-       fprintf(stderr, "fork failed\n");
-       exit(1);
-   } else if (rc == 0) {
-       // child: redirect standard output to a file
-       close(STDOUT_FILENO);
-       open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+int main(void) {
 
-       // now exec "wc"...
-       char *myargs[3];
-       myargs[0] = strdup("wc"); // program: wc (word count)
-       myargs[1] = strdup("p4.c"); // arg: file to count
-       myargs[2] = NULL; // mark end of array
-       execvp(myargs[0], myargs); // runs word count
-       } else {
-       // parent goes down this path (main)
-       int rc_wait = wait(NULL);
-  }
- return 0;
- }
+
+
+int rc = fork();
+	if (rc < 0) {
+	// fork failed
+	fprintf(stderr, "fork failed\n");
+	exit(1);
+	} else if (rc == 0) {
+		// child: redirect standard output to a file
+		printf("(hallo) hello, I am child (pid:%d)\n", (int) getpid());
+	} else {
+		// parent goes down this path (main)
+		printf("(goodbye) hello, I am parent of %d (pid:%d)\n", rc, (int) getpid());
+	}
+	return 0;
+}
