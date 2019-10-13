88429aa8f7eb6da4dabc270b9dc6e6ed693f063f
Ismail Zeybek <isyzeybek@outlook.com>
2019-10-13 14:43:21 +0200

    execl purpose included

--- 02_Task/api_task4.c
+++ 02_Task/api_task4.c
@@ -6,6 +6,9 @@
  #include <sys/wait.h>
 
  int main(void) {
+	printf("helloApiTask4.c:\n");
+	printf("	My PID %d.\n", getpid());
+
  int rc = fork();
  if (rc < 0) {
  	// fork failed
@@ -13,16 +16,24 @@
  	exit(1);
  } else if (rc == 0) {
 	printf("hello, I am child (pid:%d)\n", (int) getpid());
+	printf("---->");
 
  	// now exec "wc"...
 	char *myargs[3];
-	myargs[0] = strdup("/bin/ls"); // program: wc (word count)
-	myargs[1] = NULL; // arg: file to count
-	execvp(myargs[0], myargs); // runs word count
+	myargs[0] = strdup("/bin/ls"); // program: ls command
+	myargs[1] = "-l"; // arg: list all files
+	myargs[2] = NULL; // end of arguments/file
+
+	//With L: comma separated arguments
+	execl(myargs[0], "-l",NULL);
+	//With V: Vector (i.e., an array of strings)
+	//execvp(myargs[0], myargs); // runs ls -l command
+
 } else {
 	// parent goes down this path (main)
 	int rc_wait = wait(NULL);
-	printf("hello, I am parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid());
+	printf("hello, I am parent of %d (rc_wait: %d) (pid:%d)\n", rc, rc_wait, (int) getpid());
 }
+printf("i am executed :D\n");
 return 0;
 }
