f98d1e50c324100428520f2de224778d1f3f9b1d
Ismail Zeybek <isyzeybek@outlook.com>
2019-10-13 14:09:15 +0200

    fully prepared for api_Task4

--- 02_Task/api_task4.c
+++ 02_Task/api_task4.c
@@ -5,26 +5,24 @@
  #include <fcntl.h>
  #include <sys/wait.h>
 
- int main(int argc, char *argv[]) {
+ int main(void) {
  int rc = fork();
  if (rc < 0) {
  	// fork failed
  	fprintf(stderr, "fork failed\n");
  	exit(1);
  } else if (rc == 0) {
- 	// child: redirect standard output to a file
- 	close(STDOUT_FILENO);
- 	open("./task2.txt", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+	printf("hello, I am child (pid:%d)\n", (int) getpid());
 
  	// now exec "wc"...
 	char *myargs[3];
-	myargs[0] = strdup("wc"); // program: wc (word count)
-	myargs[1] = strdup("p4.c"); // arg: file to count
-	myargs[2] = NULL; // mark end of array
+	myargs[0] = strdup("/bin/ls"); // program: wc (word count)
+	myargs[1] = NULL; // arg: file to count
 	execvp(myargs[0], myargs); // runs word count
 } else {
 	// parent goes down this path (main)
 	int rc_wait = wait(NULL);
+	printf("hello, I am parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid());
 }
 return 0;
 }
