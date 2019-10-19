b8571d8b72efc469c94c045708eeb08db225f64d
Ismail Zeybek <isyzeybek@outlook.com>
2019-10-19 16:04:04 +0200

    prepared contextSwitch for its measurement

--- 02_Task/contexSwitch.c
+++ 02_Task/contexSwitch.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,29 +8,67 @@
 #include <sched.h>
 
 
-
 int main(void) {
-int fd[2];
-pid_t childpid;
+
+cpu_set_t set;
+int parentCPU, childCPU;
+
+int pipe1[2];
+int pipe2[2];
+parentCPU = 0;
+childCPU = 0;
+
 char string[] = "Hello new Process!\n";
 char readbuffer[80];
+char string2[] = "my name is Isy";
+CPU_ZERO(&set);
 
-pipe(fd);
-if((childpid = fork()) == -1){
-	perror("fork");
+
+pipe(pipe1);
+pipe(pipe2);
+int ret = fork();
+
+
+if(ret == -1){
+	perror("fork failed");
 	exit(1);
 }
 
-if (childpid == 0) {
-	// child: redirect standard output to a file
-	close(fd[1]); //close output
-	read(fd[0], readbuffer, sizeof(string)+1);
+if (ret == 0) {
+	CPU_SET(childCPU, &set);
+        sched_setaffinity(getpid(), sizeof(set), &set);
+
+
+	//read from parent through pipe1
+	close(pipe1[1]); //close output
+	read(pipe1[0], readbuffer, sizeof(string)+1);
 	printf("Received string: %s", readbuffer);
+
+
+	//write to parent through pipe2
+	close(pipe2[0]);
+	write(pipe2[1], string2, strlen(string2)+1);
+
 	exit(0);
+
 } else { // parent
-	close(fd[0]); //close input
-	write(fd[1],string, strlen(string)+1);
-	wait(0); //wait for child, so process shutdowns
+
+	CPU_SET(parentCPU, &set);
+ 	sched_setaffinity(getpid(), sizeof(set), &set);
+
+	//write to child through pipe1
+	close(pipe1[0]); //close input
+	write(pipe1[1],string, strlen(string)+1);
+
+
+	//read from child through pipe2
+	char string3[80];
+	close(pipe2[1]);
+	read(pipe2[0], string3, sizeof(string2)+1);
+	printf("Received string2 %s\n", string2); 
+
+	wait(NULL); //wait for child, so process shutdowns
+
 }
 return 0;
 }
