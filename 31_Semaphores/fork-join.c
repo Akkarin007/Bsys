b4309e45c03833f2bccc8ac49199c74219ffa87b
Ismail Zeybek <isyzeybek@outlook.com>
2020-01-05 15:05:05 +0100

    31_completed task 1 and 2

--- 31_Semaphores/fork-join.c
+++ 31_Semaphores/fork-join.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-#include "common_threads.h"
+#include <semaphore.h>
 
 sem_t s; 
 
 void *child(void *arg) {
     printf("child\n");
     // use semaphore here
+    sleep(1);
+    sem_post(&s); // signal here: child is done
     return NULL;
 }
 
@@ -15,8 +17,14 @@ int main(int argc, char *argv[]) {
     pthread_t p;
     printf("parent: begin\n");
     // init semaphore here
-    Pthread_create(&p, NULL, child, NULL);
+
+    sem_init(&s, 0, 0); // initialize s to the value 1
+    
+    pthread_create(&p, NULL, child, NULL);
     // use semaphore here
+    sem_wait(&s); // wait here for child
+
+    
     printf("parent: end\n");
     return 0;
 }
